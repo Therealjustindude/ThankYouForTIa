6c86647b81141bdb51f3125c31b9a82c8a0916f5
therealjustindude <therealjustindude@gmail.com>
2020-09-15 15:10:52 -0500

    db schema and seed data migrated

--- thankyoufortia-backend/db/migrate/20200915194906_create_menu_items.rb
+++ thankyoufortia-backend/db/migrate/20200915194906_create_menu_items.rb
@@ -2,7 +2,7 @@ class CreateMenuItems < ActiveRecord::Migration[6.0]
   def change
     create_table :menu_items do |t|
       t.string :title
-      t.string :ingredient
+      t.string :ingredients
       t.string :category
 
       t.timestamps
