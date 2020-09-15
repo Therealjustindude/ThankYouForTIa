05f407616c60ba10297d4528ba267016614b7930
therealjustindude <therealjustindude@gmail.com>
2020-09-15 16:56:41 -0500

    finish api auth setup and configuration

--- thankyoufortia-backend/config/routes.rb
+++ thankyoufortia-backend/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  
   resources :orders
   resources :menu_selections
   resources :menu_items
 
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 
 end
