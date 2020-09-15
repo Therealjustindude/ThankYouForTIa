f02615972a4990489cc26435891fa8963da08d62
therealjustindude <therealjustindude@gmail.com>
2020-09-15 17:42:04 -0500

    devise and user deleted

--- thankyoufortia-backend/config/routes.rb
+++ thankyoufortia-backend/config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   resources :menu_selections
   resources :menu_items
 
-  namespace :api do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-    end
-  end
+
 
 end
