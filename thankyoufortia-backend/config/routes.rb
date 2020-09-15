c4fb0951a2315de0f9d94cd71525b184e630ad61
therealjustindude <therealjustindude@gmail.com>
2020-09-15 15:33:20 -0500

    devise user deleted

--- thankyoufortia-backend/config/routes.rb
+++ thankyoufortia-backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :menu_selections
   resources :menu_items
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+ 
 end
