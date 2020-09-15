7ee78c8d86c07ebac1ff58ec435e424b09dcee07
therealjustindude <therealjustindude@gmail.com>
2020-09-15 16:19:10 -0500

    Authentication controller created, application controller has auth request func, user routes created

--- thankyoufortia-backend/config/routes.rb
+++ thankyoufortia-backend/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   resources :orders
   resources :menu_selections
   resources :menu_items
 
- 
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
