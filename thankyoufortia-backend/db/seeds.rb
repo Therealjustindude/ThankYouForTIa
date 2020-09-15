a2025af66d8dbf8f269be65aca2a68f76cea31fe
therealjustindude <therealjustindude@gmail.com>
2020-09-15 13:44:08 -0500

    front and back end connected

--- thankyoufortia-backend/db/seeds.rb
+++ thankyoufortia-backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Justin Davies", email: "justin@gmail.com", password:"1234")
+User.create(name: "Cinthia Davies", email: "cinthia@gmail.com", password:"1234")
+
+
+Menu.create(title: "Lomo Saltado", ingredients: "Thinly sliced beef steak, onions, tomatoes, french fries, soy sauce, rice, vinegar", item_type: "Dinner")
+Menu.create(title: "Ceviche", ingredients: "Fish as food, Key lime, Lemon, Onion, Peppers, Coriander", item_type: "Dinner")
+Menu.create(title: "Tallarines Verde", ingredients: "Thinly sliced beef, pasta in a green sauce", item_type: "Dinner")
+Menu.create(title: "Picarones", ingredients: "Chancaca syrup, Squash, Sweet potatoes", item_type: "Dessert")
+Menu.create(title: "Alfajores", ingredients: "Carmel sauce inbetween pastry cookies covered in powdered sugar", item_type: "Dessert")
+Menu.create(title: "Inca Kola", ingredients: "CARBONATED WATER, HIGH FRUCTOSE CORN SYRUP AND/OR SUCROSE, CITRIC ACID, SODIUM BENZOATE (TO PROTECT TASTE), CAFFEINE, ARTIFICIAL AND NATURAL FLAVORS, YELLOW #5.", item_type: "Drink")
